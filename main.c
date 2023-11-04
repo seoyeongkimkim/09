6d34ff6b232634a16d6db802879da56daef87b0d
seoyeongkimkim <144990861+seoyeongkimkim@users.noreply.github.com>
2023-11-04 23:32:42 +0900

    02-2

--- main.c
+++ main.c
@@ -7,15 +7,19 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i;
+	int i, average;
+	int sum;
 	int grade[SIZE];
 	printf("5명의 점수를 입력하세요.\n");
 	
+	sum=0;
 	for(i=0;i<SIZE;i++)
-	  scanf("%d", &grade[i]);
+	  {scanf("%d", &grade[i]);
+	   sum+=grade[i];
+	   }
 	
-	for(i=0;i<SIZE;i++)
-	  printf("grade[%d] = %d\n", i, grade[i]);
+	average=sum/SIZE;
+	printf("score average : %i\n", average);
 	
 	return 0;
 }
